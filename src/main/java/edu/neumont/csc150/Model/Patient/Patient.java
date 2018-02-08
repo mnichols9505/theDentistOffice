a514f839c0949757b70f8a922d553e6ce9295aef
mnichols9505 <33160181+mnichols9505@users.noreply.github.com>
2018-02-08 16:45:58 -0700

    update

--- src/main/java/edu/neumont/csc150/Model/Patient/Patient.java
+++ src/main/java/edu/neumont/csc150/Model/Patient/Patient.java
@@ -2,6 +2,10 @@ package edu.neumont.csc150.Model.Patient;
 
 public interface Patient {
 
+    public static void main(String[] args) {
+
+    }
+
     public String getFirstName();
 
     public void setFirstName(String firstName);
